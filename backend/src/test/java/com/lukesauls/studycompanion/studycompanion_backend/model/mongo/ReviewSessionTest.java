ddd3306dbb48061d0c168a789006692a37f30978
Luke Sauls <luke.sauls279@gmail.com>
2025-11-13 08:55:23 -0700

    created ReviewSessionTest.java and all tests are passing, also took out all setters from ReviewSession for security except setScore for testing

---
+++ backend/src/test/java/com/lukesauls/studycompanion/studycompanion_backend/model/mongo/ReviewSessionTest.java
@@ -0,0 +1,60 @@
+package com.lukesauls.studycompanion.studycompanion_backend.model.mongo;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ReviewSessionTest {
+
+    private String deckName;
+    private int score;
+    private int cardsReviewed;
+    private int correctAnswers;
+
+    @BeforeEach
+    void setUp() {
+        deckName = "Sample Deck";
+        score = 85;
+        cardsReviewed = 20;
+        correctAnswers = 17;
+    }
+
+    @Test
+    void testDefaultConstructor() {
+        ReviewSession session = new ReviewSession();
+        assertNotNull(session);
+        assertNotNull(session.getId());
+        assertNotNull(session.getDate());
+        assertNull(session.getDeckName());
+        assertEquals(0, session.getScore());
+        assertEquals(0, session.getCardsReviewed());
+        assertEquals(0, session.getCorrectAnswers());
+    }
+
+    @Test
+    void testParameterizedConstructor() {
+        ReviewSession session = new ReviewSession(deckName, score, cardsReviewed, correctAnswers);
+        assertNotNull(session);
+        assertNotNull(session.getId());
+        assertNotNull(session.getDate());
+        assertEquals(deckName, session.getDeckName());
+        assertEquals(score, session.getScore());
+        assertEquals(cardsReviewed, session.getCardsReviewed());
+        assertEquals(correctAnswers, session.getCorrectAnswers());
+    }
+    
+    @Test
+    void testToString() {
+        ReviewSession session = new ReviewSession(deckName, score, cardsReviewed, correctAnswers);
+        String expectedString = "ReviewSession{" +
+                "id='" + session.getId() + '\'' +
+                ", date=" + session.getDate() +
+                ", deckName='" + deckName + '\'' +
+                ", score=" + score +
+                ", cardsReviewed=" + cardsReviewed +
+                ", correctAnswers=" + correctAnswers +
+                '}';
+        assertEquals(expectedString, session.toString());
+    }
+}
