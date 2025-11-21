31fe305236f4c91c145db8ccf2c75ed6ffff0479
William Lyman <luke.sauls279@gmail.com>
2025-11-20 21:47:13 -0700

    took out unused imports in DeckCacheTest.java

--- backend/src/test/java/com/lukesauls/studycompanion/studycompanion_backend/model/redis/DeckCacheTest.java
+++ backend/src/test/java/com/lukesauls/studycompanion/studycompanion_backend/model/redis/DeckCacheTest.java
@@ -6,8 +6,6 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.UUID;
-import java.beans.Transient;
-import java.time.LocalDateTime;
 
 public class DeckCacheTest {
 
