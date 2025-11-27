6bcd1a738376137e2c5fbddc2a1d61e7f10b6dc8
William Lyman <luke.sauls279@gmail.com>
2025-11-26 18:21:48 -0700

    took out @Repository to have cleaner more modern style and deleted custom method from CachedDecksRepository.java since it did the same thing as the inherited methods

--- backend/src/main/java/com/lukesauls/studycompanion/studycompanion_backend/repository/redis/CachedDecksRepository.java
+++ backend/src/main/java/com/lukesauls/studycompanion/studycompanion_backend/repository/redis/CachedDecksRepository.java
@@ -2,14 +2,8 @@ package com.lukesauls.studycompanion.studycompanion_backend.repository.redis;
 
 import com.lukesauls.studycompanion.studycompanion_backend.model.redis.CachedDecks;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 import java.util.UUID;
 
-@Repository
 public interface CachedDecksRepository extends CrudRepository<CachedDecks, UUID> {
 
-    /**
-     * Check if cached decks exist for a user
-     */
-    boolean existsByUserId(UUID userId);
 }
