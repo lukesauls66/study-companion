a68689ecfbc1205e04e3c114c4792814e551637c
Luke Sauls <luke.sauls279@gmail.com>
2025-11-29 11:56:39 -0700

    edited UserDto to have a create, update, and change password record

--- backend/src/main/java/com/lukesauls/studycompanion/studycompanion_backend/dto/UserDto.java
+++ backend/src/main/java/com/lukesauls/studycompanion/studycompanion_backend/dto/UserDto.java
@@ -1,3 +1,11 @@
 package com.lukesauls.studycompanion.studycompanion_backend.dto;
 
-public record UserDto(String email, String name, String username, String password) {}
+import jakarta.validation.constraints.NotNull;
+
+public class UserDto{
+    public record Create(@NotNull String email, @NotNull String name, @NotNull String username, @NotNull String password) {}
+
+    public record Update(String email, String name, String username) {}
+
+    public record ChangePassword(@NotNull String currPassword, @NotNull String newPassword) {}
+}
