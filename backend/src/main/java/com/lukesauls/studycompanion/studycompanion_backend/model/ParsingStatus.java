fdd08f49720e2b176cfbf425a80a14032cfce58d
Luke Sauls <luke.sauls279@gmail.com>
2026-01-24 00:02:09 -0700

    deleted old spring boot backend, and changed java version

--- backend/src/main/java/com/lukesauls/studycompanion/studycompanion_backend/model/ParsingStatus.java
+++
@@ -1,8 +0,0 @@
-package com.lukesauls.studycompanion.studycompanion_backend.model;
-
-public enum ParsingStatus {
-    PENDING,
-    PROCESSING,
-    COMPLETED,
-    FAILED
-}
--- backend/src/main/java/com/lukesauls/studycompanion/studycompanion_backend/repository/redis/CachedDecksRepository.java
+++
@@ -1,9 +0,0 @@
-package com.lukesauls.studycompanion.studycompanion_backend.repository.redis;
-
-import com.lukesauls.studycompanion.studycompanion_backend.model.redis.CachedDecks;
-import org.springframework.data.repository.CrudRepository;
-import java.util.UUID;
-
-public interface CachedDecksRepository extends CrudRepository<CachedDecks, UUID> {
-
-}
