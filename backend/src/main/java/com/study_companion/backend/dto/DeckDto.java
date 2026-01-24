d0c637664d9433b37f644e57a1fd039a4a1f80ca
Luke Sauls <luke.sauls279@gmail.com>
2026-01-24 02:10:54 -0700

    created data transfer objects and enums

---
+++ backend/src/main/java/com/study_companion/backend/dto/DeckDto.java
@@ -0,0 +1,10 @@
+package com.study_companion.backend.dto;
+
+import java.util.UUID;
+import jakarta.validation.constraints.NotNull;
+
+public class DeckDto{
+    public record Create(@NotNull UUID userId, @NotNull String title, @NotNull String description) {}
+
+    public record Update(String title, String description) {}
+}
