b38cb0bbfed49923fffa3d76ddece27e6db4c797
Luke Sauls <luke.sauls279@gmail.com>
2026-01-24 02:10:16 -0700

    created data transfer objects and enums

---
+++ backend/src/main/java/com/study_companion/backend/model/FileType.java
@@ -0,0 +1,7 @@
+package com.study_companion.backend.model;
+
+public enum FileType {
+    PNG,
+    JPEG,
+    JPG
+}
---
+++ backend/src/main/java/com/study_companion/backend/model/ParsingStatus.java
@@ -0,0 +1,8 @@
+package com.study_companion.backend.model;
+
+public enum ParsingStatus {
+    PENDING,
+    PROCESSING,
+    COMPLETED,
+    FAILED
+}
